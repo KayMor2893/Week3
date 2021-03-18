9742febf74d2de1cda59a44ce6f2e432246759fc
KayMor2893 <80891668+KayMor2893@users.noreply.github.com>
2021-03-19 10:44:50 +1100

    Update time-signal.sh

--- time-signal.sh
+++ time-signal.sh
@@ -1,16 +1,17 @@
 # save as time-signal.sh
-# gives a time signal based on test
+# gives no chime when minutes are less than 20, one chime if greater than 20 but less than 40 and two chimes if greater than 40 with one second sleep
 time=$(date +%M)
-count=20
-count2=40
-if test $time -lt $count; then
-        echo -e "\a"
-        sleep 0         # sleep for one second
-elif test $time -gt $count && $time -lt $count2; then
-        echo -e "\a"
+minute=20
+minutes=40
+if test $time -lt $minute; then
+        sleep 0         # sleep for zero second and move onto next test
+elif test $time -ge $minute && test $time -lt $minutes; then
+        echo -e "\a" # one chime
         sleep 0
-else
-        echo -e "\a"
-        sleep 1
-        echo -e "\a"
+        count=1
+else # that is for all other possibilities 
+        echo -e "\a" #one chime
+        sleep 1 #one sleep
+        echo -e "\a" #one chime
+        count=2
 fi
